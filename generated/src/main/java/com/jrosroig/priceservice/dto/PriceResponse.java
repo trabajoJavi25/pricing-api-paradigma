3ff09ab258642446a1c382ecfd1515ab32039b0c
trabajojavi25@gmail.com <javier@PC-Javi.>
2025-05-01 18:30:20 +0200

    Openapi genereted launch

---
+++ generated/src/main/java/com/jrosroig/priceservice/dto/PriceResponse.java
@@ -0,0 +1,232 @@
+package com.jrosroig.priceservice.dto;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.time.OffsetDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.Nullable;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * PriceResponse
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-05-01T18:22:59.203413401+02:00[Europe/Madrid]", comments = "Generator version: 7.13.0")
+public class PriceResponse {
+
+  private @Nullable Integer productId;
+
+  private @Nullable Integer brandId;
+
+  private @Nullable Integer priceList;
+
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  private @Nullable OffsetDateTime startDate;
+
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  private @Nullable OffsetDateTime endDate;
+
+  private @Nullable Double price;
+
+  private @Nullable String currency;
+
+  public PriceResponse productId(Integer productId) {
+    this.productId = productId;
+    return this;
+  }
+
+  /**
+   * Get productId
+   * @return productId
+   */
+  
+  @Schema(name = "productId", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("productId")
+  public Integer getProductId() {
+    return productId;
+  }
+
+  public void setProductId(Integer productId) {
+    this.productId = productId;
+  }
+
+  public PriceResponse brandId(Integer brandId) {
+    this.brandId = brandId;
+    return this;
+  }
+
+  /**
+   * Get brandId
+   * @return brandId
+   */
+  
+  @Schema(name = "brandId", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("brandId")
+  public Integer getBrandId() {
+    return brandId;
+  }
+
+  public void setBrandId(Integer brandId) {
+    this.brandId = brandId;
+  }
+
+  public PriceResponse priceList(Integer priceList) {
+    this.priceList = priceList;
+    return this;
+  }
+
+  /**
+   * Get priceList
+   * @return priceList
+   */
+  
+  @Schema(name = "priceList", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("priceList")
+  public Integer getPriceList() {
+    return priceList;
+  }
+
+  public void setPriceList(Integer priceList) {
+    this.priceList = priceList;
+  }
+
+  public PriceResponse startDate(OffsetDateTime startDate) {
+    this.startDate = startDate;
+    return this;
+  }
+
+  /**
+   * Get startDate
+   * @return startDate
+   */
+  @Valid 
+  @Schema(name = "startDate", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("startDate")
+  public OffsetDateTime getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(OffsetDateTime startDate) {
+    this.startDate = startDate;
+  }
+
+  public PriceResponse endDate(OffsetDateTime endDate) {
+    this.endDate = endDate;
+    return this;
+  }
+
+  /**
+   * Get endDate
+   * @return endDate
+   */
+  @Valid 
+  @Schema(name = "endDate", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("endDate")
+  public OffsetDateTime getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(OffsetDateTime endDate) {
+    this.endDate = endDate;
+  }
+
+  public PriceResponse price(Double price) {
+    this.price = price;
+    return this;
+  }
+
+  /**
+   * Get price
+   * @return price
+   */
+  
+  @Schema(name = "price", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("price")
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public PriceResponse currency(String currency) {
+    this.currency = currency;
+    return this;
+  }
+
+  /**
+   * Get currency
+   * @return currency
+   */
+  
+  @Schema(name = "currency", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("currency")
+  public String getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(String currency) {
+    this.currency = currency;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PriceResponse priceResponse = (PriceResponse) o;
+    return Objects.equals(this.productId, priceResponse.productId) &&
+        Objects.equals(this.brandId, priceResponse.brandId) &&
+        Objects.equals(this.priceList, priceResponse.priceList) &&
+        Objects.equals(this.startDate, priceResponse.startDate) &&
+        Objects.equals(this.endDate, priceResponse.endDate) &&
+        Objects.equals(this.price, priceResponse.price) &&
+        Objects.equals(this.currency, priceResponse.currency);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(productId, brandId, priceList, startDate, endDate, price, currency);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PriceResponse {\n");
+    sb.append("    productId: ").append(toIndentedString(productId)).append("\n");
+    sb.append("    brandId: ").append(toIndentedString(brandId)).append("\n");
+    sb.append("    priceList: ").append(toIndentedString(priceList)).append("\n");
+    sb.append("    startDate: ").append(toIndentedString(startDate)).append("\n");
+    sb.append("    endDate: ").append(toIndentedString(endDate)).append("\n");
+    sb.append("    price: ").append(toIndentedString(price)).append("\n");
+    sb.append("    currency: ").append(toIndentedString(currency)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
